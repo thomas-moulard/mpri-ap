0ebe207320b71b9acd130835833d131496f8ce0c
Thomas Moulard <thomas.moulard@gmail.com>
2008-12-31 16:44:28 +0100

    Enhance type.

--- polarity.v
+++ polarity.v
@@ -1,3 +1,4 @@
+
 (* Polarity definition *)
 Require Import List.
 
@@ -32,7 +33,7 @@ Qed.
 
 Inductive typ : Type :=
 | Nat : typ
-| Fun : polarity -> typ -> typ -> typ.
+| Fun : (*polarity -> *) typ -> typ -> typ.
 
 Inductive cst : Type :=
 | Zero
@@ -55,19 +56,41 @@ Definition env := list (option (polarity * typ)).
 Definition sigma (x: cst) : typ :=
   match x with
     | Zero => Nat
-    | Succ => Fun Plus Nat Nat
-    | Addition => Fun Plus Nat (Fun Plus Nat Nat)
-    | Substraction => Fun Plus Nat (Fun Plus Nat Nat)
+    | Succ => Fun Nat Nat
+    | Addition => Fun Nat (Fun Nat Nat)
+    | Substraction => Fun Nat (Fun Nat Nat)
     | Rec => Nat (* FIXME: *)
   end.
 
-(*
+Definition inv_env (p : polarity) (e : env) : env :=
+  match p with
+    | Plus => e
+    | Minus => map (fun x => option_map
+      (fun x : (polarity * typ) => let (p, t) := x in (mul_pol Minus p, t)) x) e
+    | NonMonotonic => map (fun x : option (polarity * typ) =>
+      match x with
+        | Some (NonMonotonic, _) => x
+        | Some (Plus, _) | Some (Minus, _) | None => None
+      end) e
+  end.
+
 Inductive type : env -> trm -> typ -> Prop :=
 | type_cst : forall e t,
   match t with
     | Cst c => type e t (sigma c)
-    | Var v => forall tau, type e t tau
-    | App t1 t2 => forall tau, type e t tau
-    | Abs p t1 t2 => forall tau, type e t tau
+    | Var v => match (nth v e None) with
+                 | Some (Plus, tau) => type e t tau
+                 | Some (NonMonotonic, tau) => type e t tau
+                 | Some (Minus, tau) => forall tau, type e t tau (*FIXME: ?? *)
+                 | None => forall tau, type e t tau (*FIXME: ?? *)
+               end
+
+    | App u v => forall tau1, forall tau2, forall p, type e (Abs p (Fun tau1 tau2) u) (Fun tau1 tau2) ->
+      type (inv_env p e) v tau1 -> type e (App u v) tau2
+
+    | Abs p tau1 m => forall tau2, type e m tau2 -> type e (Abs p tau1 m) (Fun tau1 tau2)
   end.
-*)
+
+
+
+
